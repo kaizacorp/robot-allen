f41d8b0b8370c25800d9ef5d83d43bf46a952291
Kai <kaicharlton@gmail.com>
2021-04-22 15:02:01 -0600

    !gif command working, no repeats in 8

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const fetch = require("node-fetch");
 console.log("Have you met Allen?");
 
 const Discord = require("discord.js");
@@ -11,20 +12,27 @@ function readyDiscord() {
   console.log("💖");
 }
 
-const replies = [
-  "have-you-met-allen.gif",
-  "it-tastes-so-sweeeeet.gif",
-  "that's the worst pirate I've ever seen!",
-  "That's the best pirate I've ever seen!",
-  "Hahahaha",
-];
-
 client.on("message", gotMessage);
 
-function gotMessage(msg) {
+let recentGifID = [];
+async function gotMessage(msg) {
   console.log(msg.content);
   if (msg.channel.id == "834826825943089184" && msg.content === "!gif") {
-    const index = Math.floor(Math.random() * replies.length);
-    msg.channel.send(replies[index]);
+    //let tenorURL = `https://api.tenor.com/v1/search?q=allenxandria&key=${process.env.TENORKEY}&limit=50`;
+    let tenorURL = `https://g.tenor.com/v1/random?q=allenxandria&key=${process.env.TENORKEY}&limit=50`;
+    let response = await fetch(tenorURL);
+    let json = await response.json();
+    //console.log(json);
+    let index = Math.floor(Math.random() * json.results.length);
+    while (recentGifID.includes(json.results[index].id)) {
+      console.log("Repeat detected! Stop the madness!", json.results[index].id);
+      index = Math.floor(Math.random() * json.results.length);
+    }
+    recentGifID.push(json.results[index].id);
+    if (recentGifID.length > 8) {
+      recentGifID.shift();
+    }
+    console.log(recentGifID);
+    msg.channel.send(json.results[index].url);
   }
 }
