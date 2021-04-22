bcf239b6290b716b558ef23c2825fbf3ecd6e64c
Kai <kaicharlton@gmail.com>
2021-04-22 16:26:09 -0600

    punctuation in message

--- commands/tbr.js
+++ commands/tbr.js
@@ -1,6 +1,6 @@
 module.exports = async function (msg, tokens) {
   let index = Math.floor(Math.random() * tokens.length);
   msg.channel.send(
-    "You should all be reading " + tokens[index].replace(/['"]+/g, "")
+    "You should all be reading " + tokens[index].replace(/['"]+/g, "") + "."
   );
 };
