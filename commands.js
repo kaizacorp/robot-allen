38d9828fc9bfea96338c5c8b69bddd67b1a7a4dd
Kai <kaicharlton@gmail.com>
2021-04-23 08:19:42 -0600

    add tokens + username

--- commands.js
+++ commands.js
@@ -11,7 +11,7 @@ module.exports = async function (msg) {
   let command = tokens.shift();
   if (command.charAt(0) === "!") {
     command = command.substring(1);
-    console.log(command, msg.author);
+    console.log(command, tokens, msg.author.username);
     commands[command](msg, tokens);
   }
 };
