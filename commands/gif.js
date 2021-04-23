fd71a74f83378476fc56ddf081840568da03eed4
Kai <kaicharlton@gmail.com>
2021-04-22 21:41:57 -0600

    attempt to catch shift of null error?

--- commands/gif.js
+++ commands/gif.js
@@ -2,24 +2,28 @@ const fetch = require("node-fetch");
 
 let recentGifID = [];
 module.exports = async function (msg) {
-  //let tenorURL = `https://api.tenor.com/v1/search?q=allenxandria&key=${process.env.TENORKEY}&limit=50`;
-  let tenorURL = `https://g.tenor.com/v1/random?q=allenxandria&key=${process.env.TENORKEY}&limit=50`;
-  let response = await fetch(tenorURL);
-  let json = await response.json();
-  let index = Math.floor(Math.random() * json.results.length);
-  let uniqueAttempts = 0;
-  while (recentGifID.includes(json.results[index].id)) {
-    uniqueAttempts += 1;
-    //console.log("Repeat detected! Stop the madness!", json.results[index].id);
-    index = Math.floor(Math.random() * json.results.length);
-  }
-  recentGifID.push(json.results[index].id);
-  if (recentGifID.length >= 25) {
-    if (recentGifID) {
-      recentGifID.shift();
+  try {
+    //let tenorURL = `https://api.tenor.com/v1/search?q=allenxandria&key=${process.env.TENORKEY}&limit=50`;
+    let tenorURL = `https://g.tenor.com/v1/random?q=allenxandria&key=${process.env.TENORKEY}&limit=50`;
+    let response = await fetch(tenorURL);
+    let json = await response.json();
+    let index = Math.floor(Math.random() * json.results.length);
+    let uniqueAttempts = 0;
+    while (recentGifID.includes(json.results[index].id)) {
+      uniqueAttempts += 1;
+      //console.log("Repeat detected! Stop the madness!", json.results[index].id);
+      index = Math.floor(Math.random() * json.results.length);
     }
+    recentGifID.push(json.results[index].id);
+    if (recentGifID.length >= 25) {
+      if (recentGifID) {
+        recentGifID.shift();
+      }
+    }
+    //console.log(recentGifID);
+    console.log(uniqueAttempts);
+    msg.channel.send(json.results[index].url);
+  } catch (error) {
+    console.log(error);
   }
-  //console.log(recentGifID);
-  console.log(uniqueAttempts);
-  msg.channel.send(json.results[index].url);
 };
