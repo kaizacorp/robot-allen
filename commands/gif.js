dbeec88275369109aaacd44f32049a5a58f033ff
Kai <kaicharlton@gmail.com>
2021-04-22 18:48:20 -0600

    just log total unique attempts instead of all hits

--- commands/gif.js
+++ commands/gif.js
@@ -7,14 +7,17 @@ module.exports = async function (msg) {
   let response = await fetch(tenorURL);
   let json = await response.json();
   let index = Math.floor(Math.random() * json.results.length);
+  let uniqueAttempts = 0;
   while (recentGifID.includes(json.results[index].id)) {
-    console.log("Repeat detected! Stop the madness!", json.results[index].id);
+    uniqueAttempts += 1;
+    //console.log("Repeat detected! Stop the madness!", json.results[index].id);
     index = Math.floor(Math.random() * json.results.length);
   }
   recentGifID.push(json.results[index].id);
-  if (recentGifID.length > 25) {
+  if (recentGifID.length >= 25) {
     recentGifID.shift();
   }
-  console.log(recentGifID);
+  //console.log(recentGifID);
+  console.log(uniqueAttempts);
   msg.channel.send(json.results[index].url);
 };
