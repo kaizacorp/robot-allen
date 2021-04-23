c87b162d1275fac2d4795ad9b47a8914fc51852b
Kai <kaicharlton@gmail.com>
2021-04-23 08:27:37 -0600

    readd deleted log

--- commands.js
+++ commands.js
@@ -10,6 +10,7 @@ module.exports = async function (msg) {
     let command = tokens.shift();
     if (command.charAt(0) === "!") {
       command = command.substring(1);
+      console.log(command, tokens, msg.author.username);
       commands[command](msg, tokens);
     }
   }
