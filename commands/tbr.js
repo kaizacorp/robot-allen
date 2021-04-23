1303b2d47f2adf2bb96ffb0cd58283a3e300bb46
Kai <kaicharlton@gmail.com>
2021-04-22 21:12:32 -0600

    only replace when tokens exist

--- commands/tbr.js
+++ commands/tbr.js
@@ -1,8 +1,7 @@
 module.exports = async function (msg, tokens) {
   if (tokens.length === 0) {
     msg.channel.send("Robot Allen requires at least two (or more) choices");
-  }
-  if (tokens.length === 1) {
+  } else if (tokens.length === 1) {
     msg.channel.send("Do you wanna go for " + tokens + " again?");
   }
   let index = Math.floor(Math.random() * tokens.length);
