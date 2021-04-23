36b3b019304e168a61f160fc2fcb7c3870c16864
Kai <kaicharlton@gmail.com>
2021-04-23 08:24:51 -0600

    guard against shifting null

--- commands.js
+++ commands.js
@@ -1,17 +1,16 @@
-const { Message } = require("discord.js");
 const gif = require("./commands/gif.js");
 const tbr = require("./commands/tbr.js");
 
 const commands = { gif, tbr };
 
 module.exports = async function (msg) {
-  if (msg.author.bot) return; // Robot Allen doesn't speak to other bots
-  let tokens = msg.content.replace(/”/g, '"');
-  tokens = tokens.match(/(?:[^\s"]+|"[^"]*")+/g);
-  let command = tokens.shift();
-  if (command.charAt(0) === "!") {
-    command = command.substring(1);
-    console.log(command, tokens, msg.author.username);
-    commands[command](msg, tokens);
+  let tokens = msg.content;
+  if (tokens.length > 0) {
+    tokens = tokens.match(/(?:[^\s"]+|"[^"]*")+/g);
+    let command = tokens.shift();
+    if (command.charAt(0) === "!") {
+      command = command.substring(1);
+      commands[command](msg, tokens);
+    }
   }
 };
