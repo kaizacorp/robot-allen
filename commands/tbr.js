f5b00d2992bf114065d2c5fe1f61d04c6ec5b196
Kai <kaicharlton@gmail.com>
2021-04-24 09:26:11 -0600

    don't strip single quotes

--- commands/tbr.js
+++ commands/tbr.js
@@ -5,7 +5,7 @@ module.exports = async function (msg, tokens) {
     msg.channel.send("Do you wanna go for " + tokens + " again?");
   } else {
     let index = Math.floor(Math.random() * tokens.length);
-    let book = tokens[index].replace(/['"“”]+/g, "");
+    let book = tokens[index].replace(/["“”]+/g, "");
     let text = "";
 
     switch (book) {
