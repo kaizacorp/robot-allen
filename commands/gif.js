26bb9b049599e7eba617735510c7695a9901e35d
Kai <kaicharlton@gmail.com>
2021-05-20 00:27:41 -0600

    rough implentation of searching with gif command

--- commands/gif.js
+++ commands/gif.js
@@ -4,28 +4,38 @@ const fs = require("fs");
 //let recentGifID = [];
 let recentGifID = read("./commands/recentGifID.txt");
 
-module.exports = async function (msg) {
+module.exports = async function (msg, tokens) {
   try {
-    //let tenorURL = `https://api.tenor.com/v1/search?q=allenxandria&key=${process.env.TENORKEY}&limit=50`;
-    let tenorURL = `https://g.tenor.com/v1/random?q=allenxandria&key=${process.env.TENORKEY}&limit=50`;
-    let response = await fetch(tenorURL);
-    let json = await response.json();
-    let index = Math.floor(Math.random() * json.results.length);
-    //prevent repetitions of last 24 gifs
-    let uniqueAttempts = 0;
-    while (recentGifID.includes(json.results[index].id)) {
-      uniqueAttempts += 1;
-      index = Math.floor(Math.random() * json.results.length);
-    }
-    recentGifID.push(json.results[index].id);
-    if (recentGifID.length >= 25) {
-      if (recentGifID) {
-        recentGifID.shift();
+    console.log(tokens);
+    // tokens after !gif command treated as search terms added on to allenxandria tag
+    if (tokens.length > 0) {
+      let terms = "allenxandria " + tokens.join(" ");
+      let tenorURL = `https://api.tenor.com/v1/search?q=${terms}&key=${process.env.TENORKEY}&limit=1`;
+      let response = await fetch(tenorURL);
+      let json = await response.json();
+      console.log(json.results[0].url);
+      msg.channel.send(json.results[0].url);
+    } else {
+      let tenorURL = `https://g.tenor.com/v1/random?q=allenxandria&key=${process.env.TENORKEY}&limit=50`;
+      let response = await fetch(tenorURL);
+      let json = await response.json();
+      let index = Math.floor(Math.random() * json.results.length);
+      //prevent repetitions of last 24 gifs
+      let uniqueAttempts = 0;
+      while (recentGifID.includes(json.results[index].id)) {
+        uniqueAttempts += 1;
+        index = Math.floor(Math.random() * json.results.length);
       }
-    }
-    console.log(recentGifID.length, uniqueAttempts);
+      recentGifID.push(json.results[index].id);
+      if (recentGifID.length >= 25) {
+        if (recentGifID) {
+          recentGifID.shift();
+        }
+      }
+      console.log(recentGifID.length, uniqueAttempts);
 
-    msg.channel.send(json.results[index].url);
+      msg.channel.send(json.results[index].url);
+    }
   } catch (error) {
     console.log(error);
   }
