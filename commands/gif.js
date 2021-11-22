da783396ad1a9b9ccedb14c5df95ad341b052ef3
Kai <kaicharlton@gmail.com>
2021-11-22 03:11:24 -0600

    refactor search to use api

--- commands/gif.js
+++ commands/gif.js
@@ -8,62 +8,14 @@ module.exports = async function (msg, tokens) {
   try {
     // Tokens after !gif command treated as search terms added on to allenxandria tag
     if (tokens.length > 0) {
-      tokens.unshift("allenxandria");
       let terms = tokens
         .join(" ")
         .replace(/[’'"“”]+/g, "")
         .toLowerCase();
-      // Check for unindexed terms before making request to Tenor
-      // TODO: refactor into some kind of database?
-      let gif = "";
-      if (terms.includes("bagel") || terms.includes("you are a bagel")) {
-        gif = "https://tenor.com/bFDiN.gif";
-        msg.channel.send(gif);
-        return;
-      }
-      if (
-        terms.includes("chewbacca") ||
-        terms.includes("perturbed") ||
-        terms.includes("noise")
-      ) {
-        /* Errors with this particular gif being indexed on Tenor
-        gif = "";
-        msg.channel.send(gif);
-        return;
-        */
-      }
-      if (terms.includes("croissant") || terms.includes("i am eating")) {
-        /* Errors with this particular gif being indexed on Tenor
-        gif = "";
-        msg.channel.send(gif);
-        return;
-        */
-      }
-      /* gif for 'true' reject by Tenor
-      if (terms.includes("true") || terms.includes("all true")) {
-        gif = "";
-        msg.channel.send(gif);
-        return;
-      }*/
-      let tenorURL = `https://api.tenor.com/v1/search?q=${terms}&key=${process.env.TENORKEY}&limit=1&contentfilter=medium&locale=en_US&media_filter=minimal`;
-      let response = await fetch(tenorURL);
+      let apiURL = `http://localhost:3000/search?q=${terms}`;
+      let response = await fetch(apiURL);
       let json = await response.json();
-      if (!json.results || !json.results[0]) {
-        console.log("Error with response json.");
-        return;
-      } else {
-        gif = json.results[0].url;
-      }
-      // If no matching terms, Tenor will currently select the discworld gif.
-      // Assuming this, the 'default' gif can be set to the boosh gif.
-      let isDiscworldSearch =
-        terms.includes("disc") || terms.includes("discworld");
-
-      if (!isDiscworldSearch && gif === "https://tenor.com/by3kp.gif") {
-        gif = "https://tenor.com/view/allenxandria-boosh-gif-20708353";
-      }
-
-      msg.channel.send(gif);
+      msg.channel.send(json[0].url);
     } else {
       let apiURL = "http://localhost:3000/random";
       let response = await fetch(apiURL);
