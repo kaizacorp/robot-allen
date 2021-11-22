0f50cbc7b8802d264ea997100eb153f06626f047
Kai <kaicharlton@gmail.com>
2021-11-22 03:16:10 -0600

    param as tags not q

--- commands/gif.js
+++ commands/gif.js
@@ -12,7 +12,7 @@ module.exports = async function (msg, tokens) {
         .join(" ")
         .replace(/[’'"“”]+/g, "")
         .toLowerCase();
-      let apiURL = `http://localhost:3000/search?q=${terms}`;
+      let apiURL = `http://localhost:3000/search?tags=${terms}`;
       let response = await fetch(apiURL);
       let json = await response.json();
       msg.channel.send(json[0].url);
