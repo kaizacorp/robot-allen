c0fe0e4c57108b93ef658d17df14b4cc5660ae89
Kai <kaicharlton@gmail.com>
2021-11-23 13:20:12 -0600

    missing fetch

--- commands/count.js
+++ commands/count.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const fetch = require("node-fetch");
 module.exports = async function (msg, tokens, command) {
   let apiURL = "http://localhost:3000/count";
   let response = await fetch(apiURL);
