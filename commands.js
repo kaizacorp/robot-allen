ee47c9ead4e77aeb8462963417ddd51b1f9e1d6d
Kai <kaicharlton@gmail.com>
2021-11-29 00:45:13 -0600

    format log

--- commands.js
+++ commands.js
@@ -26,7 +26,13 @@ module.exports = async function (msg) {
         commands[command](msg, tokens);
       }
     } else if (command.toLowerCase() === "allenbot") {
-      console.log("Allenbot", tokens, msg.author.username, "@", msg.guild.name);
+      console.log(
+        "Allenbot",
+        tokens.join(" "),
+        msg.author.username,
+        "@",
+        msg.guild.name
+      );
       commands["gif"](msg, []);
     }
   }
