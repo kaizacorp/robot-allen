7f426a46a082fb875e12293cc9d61e28e263acb6
Kai <kaicharlton@gmail.com>
2021-11-29 01:01:15 -0600

    refactor / tidy comments

--- commands/gif.js
+++ commands/gif.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 
 module.exports = async function (msg, tokens) {
   try {
-    // Tokens after !gif command treated as search terms added on to allenxandria tag
+    // Tokens after !gif command or Allenbot treated as search terms
     if (tokens.length > 0) {
       let terms = tokens
         .join(" ")
@@ -11,25 +11,18 @@ module.exports = async function (msg, tokens) {
       let apiURL = `http://localhost:3000/search?tags=${terms}`;
       let response = await fetch(apiURL);
       let json = await response.json();
-      let gif;
+      let gif = "https://tenor.com/view/allenxandria-boosh-gif-20708353";
       if (json.length > 0) {
         gif = json[0].url;
-      } else {
-        // default gif
-        gif = "https://tenor.com/view/allenxandria-boosh-gif-20708353";
       }
       msg.channel.send(gif);
     } else {
       let apiURL = "http://localhost:3000/random";
       let response = await fetch(apiURL);
       let json = await response.json();
-      let gif;
-
+      let gif = "https://tenor.com/view/allenxandria-boosh-gif-20708353";
       if (Object.keys(json).length !== 0) {
         gif = json.url;
-      } else {
-        // default gif
-        gif = "https://tenor.com/view/allenxandria-boosh-gif-20708353";
       }
       msg.channel.send(gif);
     }
