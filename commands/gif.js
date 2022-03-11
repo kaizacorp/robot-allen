fb1ee66f148656999bae2f5c5e8422679f8e2164
Kai <kaicharlton@gmail.com>
2022-03-11 13:24:42 -0600

    remove let

--- commands/gif.js
+++ commands/gif.js
@@ -11,7 +11,7 @@ module.exports = async function (msg, tokens) {
         .join(" ")
         .replace(/[’'"“”]+/g, "")
         .toLowerCase();
-      let apiURL = `http://localhost:3000/search?tags=${terms}`;
+      apiURL = `http://localhost:3000/search?tags=${terms}`;
     }
     let response = await fetch(apiURL);
     let json = await response.json();
