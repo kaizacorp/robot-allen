371bb8806995df5c29a445a018f8ac848925a747
Kai <kaicharlton@gmail.com>
2023-02-01 11:07:59 -0600

    consistently denote params for commands

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports = async function (msg, tokens, command) {
     reply =
       "This is the `!gif` command.\n" +
       "You can send `!gif` for a random Library of Allenxandria GIF (lovingly crafted by the one and only Evie!)\n" +
-      "You can also send `!gif term1 term2 ...` to search for a specific Allen gif.";
+      "You can also send `!gif <term1> <term2> ...` to search for a specific Allen gif.";
   } else if (tokens[0] === "help") {
     title = "!help command usage";
     reply =
@@ -23,14 +23,14 @@ module.exports = async function (msg, tokens, command) {
     title = "!tbr command usage";
     reply =
       "This is the `!tbr` command.\n" +
-      "You can send `!tbr 'choice1' 'choice2' ...` to have Allenbot decide your TBR for you!\n" +
+      "You can send `!tbr <choice1> <choice2> ...` to have Allenbot decide your TBR for you!\n" +
       "Allenbot has special responses for certain titles, see if you can find them!\n" +
       "You can send `!tbr <goodreads/storygraph shelf link>` to have Allenbot randomly select a book from your shelf!";
   } else if (tokens[0] === "tags") {
     title = "!tag command usage";
     reply =
       "This is the `!tags` command.\n" +
-      "You can send `!tags gif-url` to have Allenbot tell you which tags are associated with a given gif URL.";
+      "You can send `!tags <gif url>` to have Allenbot tell you which tags are associated with a given gif URL.";
   } else {
     title = "Unknown command?";
     reply =
