5dafb096074297bad3c91cde4979400840fb2a93
Kai <kaicharlton@gmail.com>
2023-02-03 10:33:39 -0600

    refactor for clearer error messages to user

--- commands/tags.js
+++ commands/tags.js
@@ -3,20 +3,23 @@ require("dotenv").config();
 
 module.exports = async function (msg, tokens) {
   try {
-    // default API endpoint as random gif
-    let apiURL = `${process.env.API_URL}/random`;
-    // if tokens exist, use on tags endpoint
-    if (tokens.length > 0) {
-      let url = tokens;
-      apiURL = `${process.env.API_URL}/tags?url=${url}`;
+    let tagsMessage = "Invalid URL to perform tag search.";
+    if (!tokens || tokens.length === 0) {
+      msg.channel.send(tagsMessage);
+      return;
     }
-    let response = await fetch(apiURL);
-    let json = await response.json();
-    let tags = "No matching tags found for given url";
+
+    const url = tokens;
+    const apiURL = `${process.env.API_URL}/tags?url=${url}`;
+    const response = await fetch(apiURL);
+    const json = await response.json();
+
+    tagsMessage = "No matching tags found for given URL.";
     if (Object.keys(json).length !== 0 && json.tags !== "") {
-      tags = json.tags;
+      tagsMessage = json.tags;
     }
-    msg.channel.send("Tags: " + tags);
+
+    msg.channel.send("Tags: " + tagsMessage);
   } catch (error) {
     console.log(error);
   }
